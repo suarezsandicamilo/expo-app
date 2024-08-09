81bd765041da5b73aeaa8c507130f29f738aab24
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-08-09 17:16:55 -0600

    Add feedback to select task

--- src/tasks/select_task.tsx
+++ src/tasks/select_task.tsx
@@ -10,13 +10,13 @@ import { Animated, Image, StyleSheet, View } from 'react-native';
 
 // App
 
-import { Card, SpeechView } from '@/components';
+import { Card } from '@/components';
 
-import { useSound } from '@/hooks';
+import { useSound, useSpeech } from '@/hooks';
 
-import { delay, shuffle } from '@/_';
+import { shuffle } from '@/_';
 
-import { getImage, images } from '@/../assets/images';
+import { getImage, ImageSource } from '@/../assets/images';
 
 const styles = StyleSheet.create({
   container_1: {
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
 
 type Option = {
   text: string;
-  image: keyof typeof images;
+  image: ImageSource;
   correct: boolean;
 };
 
@@ -65,29 +65,36 @@ type Props = {
   next: () => void;
   instruction?: string;
   text?: string;
-  image?: keyof typeof images;
+  image?: ImageSource;
+  feedback?: {
+    correct: string;
+    incorrect: string;
+  };
   options?: Option[];
 };
 
 export const SelectTask = (props: Props) => {
   const anim = useRef(new Animated.Value(-500)).current;
 
+  const { speak } = useSpeech();
+
   return (
     <View style={styles.container_1}>
       <View style={styles.container_2}>
-        <Card>
-          <SpeechView
-            speech={`${props.text} ${props.instruction}`}
-            onPress={() => {
-              Animated.timing(anim, {
-                duration: 250,
-                toValue: 0,
-                useNativeDriver: true,
-              }).start();
-            }}
-          >
-            <Image style={styles.image_1} source={getImage(props.image)} />
-          </SpeechView>
+        <Card
+          onPress={async () => {
+            await speak(props.text);
+
+            await speak(props.instruction);
+
+            Animated.timing(anim, {
+              duration: 250,
+              toValue: 0,
+              useNativeDriver: true,
+            }).start();
+          }}
+        >
+          <Image style={styles.image_1} source={getImage(props.image)} />
         </Card>
       </View>
       <Animated.View
@@ -106,29 +113,34 @@ const InSelectTask = (props: Props) => {
 
   const incorrect = useSound('incorrect');
 
+  const { speak } = useSpeech();
+
   useEffect(() => {
     setOptions(shuffle(props.options ?? []));
   }, []);
 
   return options.map((option) => {
     return (
-      <Card key={option.text} size={128}>
-        <SpeechView
-          speech={option.text}
-          onPress={() => {
-            if (option.correct) {
-              correct.play();
+      <Card
+        key={option.text}
+        size={128}
+        onPress={async () => {
+          await speak(option.text);
 
-              delay(1250).then(() => {
-                props.next();
-              });
-            } else {
-              incorrect.play();
-            }
-          }}
-        >
-          <Image style={styles.image_2} source={getImage(option.image)} />
-        </SpeechView>
+          if (option.correct) {
+            await speak(props.feedback?.correct);
+
+            await correct.play();
+
+            props.next();
+          } else {
+            await speak(props.feedback?.incorrect);
+
+            await incorrect.play();
+          }
+        }}
+      >
+        <Image style={styles.image_2} source={getImage(option.image)} />
       </Card>
     );
   });
