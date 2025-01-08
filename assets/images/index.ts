d7cd4e599c0ad244eddcf0839d99c1b8dafabc81
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2025-01-08 16:46:13 -0600

    Use other sounds

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,15 +1,5 @@
 //
 
-const images = {};
+export type ImageKey = keyof typeof Images;
 
-export type ImageSource = keyof typeof images;
-
-export const getImage = (name?: ImageSource) => {
-  const def = `https://placehold.co/256x256@3x/aed581/33691e/png?text=${name}&font=Roboto`;
-
-  if (name == undefined) {
-    return { uri: def };
-  }
-
-  return images[name] ?? { uri: def };
-};
+export const Images = {} as const;
