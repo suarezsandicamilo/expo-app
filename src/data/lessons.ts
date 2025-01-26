d2a5637910fdeb22bace73d39bb0675509e91b73
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2025-01-26 13:43:37 -0600

    Move test data

--- src/data/lessons.ts
+++ src/data/lessons.ts
@@ -7,428 +7,31 @@ import { Lesson } from '@/types';
 export const Lessons: Lesson[] = [
   {
     id: 1,
-    tasks: [
-      {
-        id: 1,
-        type: 'tap',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          button: {
-            text: 'La abeja zumba.',
-          },
-          feedback: {
-            correct: '¡Buen trabajo!',
-          },
-        },
-      },
-      {
-        id: 2,
-        type: 'select',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          instructions: [
-            'Selecciona la carta de abajo que tiene la palabra que escuchaste.',
-          ],
-          button: {
-            text: 'La abeja zumba.',
-          },
-          options: [
-            {
-              text: 'Abeja',
-              image: 'abeja',
-              correct: true,
-            },
-            {
-              text: 'Avión',
-              image: 'avion',
-              correct: false,
-            },
-            {
-              text: 'Árbol',
-              image: 'arbol',
-              correct: false,
-            },
-            {
-              text: 'Ave',
-              image: 'ave',
-              correct: false,
-            },
-          ],
-          feedback: {
-            correct: '¡Buen trabajo! La palabra es abeja.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-        },
-      },
-      {
-        id: 3,
-        type: 'drag_and_drop',
-        instructions: ['Toca el botón de arriba y escucha la oración.'],
-        data: {
-          instructions: [
-            'Arrastra y suelta en el cuadro la carta de abajo que tiene la palabra que escuchaste.',
-          ],
-          button: {
-            text: 'La abeja zumba.',
-          },
-          options: [
-            {
-              text: 'Abeja',
-              image: 'abeja',
-              correct: true,
-            },
-            {
-              text: 'Avión',
-              image: 'avion',
-              correct: false,
-            },
-            {
-              text: 'Árbol',
-              image: 'arbol',
-              correct: false,
-            },
-            {
-              text: 'Ave',
-              image: 'ave',
-              correct: false,
-            },
-          ],
-          feedback: {
-            correct: '¡Buen trabajo! La palabra es abeja.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-        },
-      },
-      {
-        id: 4,
-        type: 'count',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          instructions: [
-            'Toca las burbujas y cuenta el número de palabras en la oración.',
-          ],
-          button: {
-            text: 'La abeja zumba.',
-          },
-          count: 3,
-          feedback: {
-            correct: '¡Buen trabajo! La oración tiene tres palabras.',
-          },
-        },
-      },
-      {
-        id: 5,
-        type: 'super_tap',
-        instructions: ['Toca los botones y escucha las oraciones.'],
-        data: {
-          buttons: [
-            {
-              text: 'María come manzanas.',
-            },
-            {
-              text: 'María come naranjas.',
-            },
-          ],
-          feedback: {
-            correct: [
-              '¡Buen trabajo!',
-              'En la primera oración María come manzanas.',
-              'Y en la segunda oración María come naranjas.',
-            ],
-          },
-        },
-      },
-      {
-        id: 6,
-        type: 'super_select',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          instructions: ['Selecciona la carta de abajo que formamos.'],
-          button: {
-            text: [
-              'Si a "Pan"!',
-              'Le agregamos "Da"!',
-              '¿Qué palabra formamos?',
-            ],
-          },
-          options: [
-            {
-              text: 'Panda',
-              image: 'panda',
-              correct: true,
-            },
-            {
-              text: 'Abeja',
-              image: 'abeja',
-              correct: false,
-            },
-            {
-              text: 'Avión',
-              image: 'avion',
-              correct: false,
-            },
-            {
-              text: 'Árbol',
-              image: 'arbol',
-              correct: false,
-            },
-          ],
-          feedback: {
-            correct: '¡Buen trabajo! La palabra es panda.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-        },
-      },
-      {
-        id: 7,
-        type: 'correct_incorrect',
-        instructions: ['Toca la carta de arriba'],
-        data: {
-          instruction: 'Repite conmigo',
-          instruction2: 'ana',
-          instruction3: 'come',
-          instruction4: 'banana.',
-          text: 'Las palabras, ana, y, banana, riman?',
-          feedback: {
-            correct: '¡Excelente! Muy buen trabajo.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-          words: [
-            { text: 'Ana', image: 'Ana' },
-            { text: 'Banana', image: 'Banana' },
-          ],
-          staticImage: 'Ana_Come_Banana',
-        },
-      },
-    ],
+    tasks: [],
   },
   {
     id: 2,
-    tasks: [
-      {
-        id: 1,
-        type: 'tap',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          button: {
-            text: 'La abeja zumba.',
-          },
-          feedback: {
-            correct: '¡Buen trabajo!',
-          },
-        },
-      },
-    ],
+    tasks: [],
   },
   {
     id: 3,
-    tasks: [
-      {
-        id: 1,
-        type: 'select',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          instructions: [
-            'Selecciona la carta de abajo que tiene la palabra que escuchaste.',
-          ],
-          button: {
-            text: 'La abeja zumba.',
-          },
-          options: [
-            {
-              text: 'Abeja',
-              image: 'abeja',
-              correct: true,
-            },
-            {
-              text: 'Avión',
-              image: 'avion',
-              correct: false,
-            },
-            {
-              text: 'Árbol',
-              image: 'arbol',
-              correct: false,
-            },
-            {
-              text: 'Ave',
-              image: 'ave',
-              correct: false,
-            },
-          ],
-          feedback: {
-            correct: '¡Buen trabajo! La palabra es abeja.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-        },
-      },
-    ],
+    tasks: [],
   },
   {
     id: 4,
-    tasks: [
-      {
-        id: 1,
-        type: 'drag_and_drop',
-        instructions: ['Toca el botón de arriba y escucha la oración.'],
-        data: {
-          instructions: [
-            'Arrastra y suelta en el cuadro la carta de abajo que tiene la palabra que escuchaste.',
-          ],
-          button: {
-            text: 'La abeja zumba.',
-          },
-          options: [
-            {
-              text: 'Abeja',
-              image: 'abeja',
-              correct: true,
-            },
-            {
-              text: 'Avión',
-              image: 'avion',
-              correct: false,
-            },
-            {
-              text: 'Árbol',
-              image: 'arbol',
-              correct: false,
-            },
-            {
-              text: 'Ave',
-              image: 'ave',
-              correct: false,
-            },
-          ],
-          feedback: {
-            correct: '¡Buen trabajo! La palabra es abeja.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-        },
-      },
-    ],
+    tasks: [],
   },
   {
     id: 5,
-    tasks: [
-      {
-        id: 1,
-        type: 'count',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          instructions: [
-            'Toca las burbujas y cuenta el número de palabras en la oración.',
-          ],
-          button: {
-            text: 'La abeja zumba.',
-          },
-          count: 3,
-          feedback: {
-            correct: '¡Buen trabajo! La oración tiene tres palabras.',
-          },
-        },
-      },
-    ],
+    tasks: [],
   },
   {
     id: 6,
-    tasks: [
-      {
-        id: 1,
-        type: 'super_tap',
-        instructions: ['Toca los botones y escucha las oraciones.'],
-        data: {
-          buttons: [
-            {
-              text: 'María come manzanas.',
-            },
-            {
-              text: 'María come naranjas.',
-            },
-          ],
-          feedback: {
-            correct: [
-              '¡Buen trabajo!',
-              'En la primera oración María come manzanas.',
-              'Y en la segunda oración María come naranjas.',
-            ],
-          },
-        },
-      },
-    ],
+    tasks: [],
   },
   {
     id: 7,
-    tasks: [
-      {
-        id: 1,
-        type: 'super_select',
-        instructions: ['Toca el botón y escucha la oración.'],
-        data: {
-          instructions: ['Selecciona la carta de abajo que formamos.'],
-          button: {
-            text: [
-              'Si a "Pan"!',
-              'Le agregamos "Da"!',
-              '¿Qué palabra formamos?',
-            ],
-          },
-          options: [
-            {
-              text: 'Panda',
-              image: 'panda',
-              correct: true,
-            },
-            {
-              text: 'Abeja',
-              image: 'abeja',
-              correct: false,
-            },
-            {
-              text: 'Avión',
-              image: 'avion',
-              correct: false,
-            },
-            {
-              text: 'Árbol',
-              image: 'arbol',
-              correct: false,
-            },
-          ],
-          feedback: {
-            correct: '¡Buen trabajo! La palabra es panda.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-        },
-      },
-    ],
-  },
-  {
-    id: 8,
-    tasks: [
-      {
-        id: 1,
-        type: 'correct_incorrect',
-        instructions: ['Toca la carta de arriba'],
-        data: {
-          instruction: 'Repite conmigo',
-          instruction2: 'ana',
-          instruction3: 'come',
-          instruction4: 'banana.',
-          text: 'Las palabras, ana, y, banana, riman?',
-          correct_answer: 'Sí',
-          feedback: {
-            correct: '¡Excelente! Muy buen trabajo.',
-            incorrect: 'Casi, intenta otra vez.',
-          },
-          words: [
-            { text: 'Ana', image: 'Ana' },
-            { text: 'Banana', image: 'Banana' },
-          ],
-          staticImage: 'Ana_Come_Banana',
-        },
-      },
-    ],
+    tasks: [],
   },
   {
     id: 9,
@@ -436,6 +39,30 @@ export const Lessons: Lesson[] = [
   },
   {
     id: 10,
+    tasks: [],
+  },
+  {
+    id: 11,
+    tasks: [],
+  },
+  {
+    id: 12,
+    tasks: [],
+  },
+  {
+    id: 13,
+    tasks: [],
+  },
+  {
+    id: 14,
+    tasks: [],
+  },
+  {
+    id: 15,
+    tasks: [],
+  },
+  {
+    id: 16,
     tasks: [],
   },
 ];
